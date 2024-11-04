658501fdb5f1f0ef250ebd0cc9212a86ed9cdb14
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-04 11:20:06 +0300

    Defined the sidebar item component

---
+++ src/features/editor/components/sidebar-item.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+
+type Props = {
+  icon: LucideIcon;
+  label: string;
+  isActive?: boolean;
+  onClick: () => void;
+};
+
+const SidebarItem = ({ icon: Icon, label, onClick, isActive }: Props) => {
+  return (
+    <Button
+      onClick={onClick}
+      className={cn(
+        "flex aspect-video size-full flex-col rounded-none p-3 py-4",
+        isActive && "bg-muted text-primary",
+      )}
+    >
+      <Icon className="size-5 shrink-0 stroke-2" />
+      <span className="mt-2 text-xs">{label}</span>
+    </Button>
+  );
+};
+
+export default SidebarItem;

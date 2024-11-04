d1f5f389f595c5b498b0e5990d2ae296f2c52bf1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-04 10:23:45 +0300

    Created and rendered the Logo component

---
+++ src/features/editor/components/logo.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="relative size-8 shrink-0">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          fill
+          className="shrink-0 transition hover:opacity-75"
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Logo from "@/features/editor/components/logo";
+
 const Navbar = () => {
   return (
     <nav className="flex h-[68px] w-full items-center gap-x-8 border-b p-4 lg:pl-[34px]">
-      Navbar
+      <Logo />
     </nav>
   );
 };

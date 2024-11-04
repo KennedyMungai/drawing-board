453354d46b583d294582c5ca611c475d0e5ff1bf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-04 14:29:42 +0300

    Added the variables for the shape options

--- src/features/editor/types.ts
+++ src/features/editor/types.ts
@@ -24,3 +24,18 @@ export type BuildEditorProps = {
 export type Editor = {
   addCircle: () => void;
 };
+
+export const FILL_COLOR = "rgba(0,0,0,1)";
+
+export const STROKE_COLOR = "rgba(0,0,0,1)";
+
+export const STROKE_WIDTH = 2;
+
+export const CIRCLE_OPTIONS = {
+  radius: 150,
+  left: 100,
+  top: 100,
+  fill: FILL_COLOR,
+  stroke: STROKE_COLOR,
+  stroke_width: STROKE_WIDTH,
+};

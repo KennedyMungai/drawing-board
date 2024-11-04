c2f93f74057ba7d53937f01f54e0255fc01f51b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-04 11:13:21 +0300

    Added the various navigation items

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -10,7 +10,14 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import Logo from "@/features/editor/components/logo";
-import { ChevronDownIcon, MousePointerClickIcon } from "lucide-react";
+import {
+  ChevronDownIcon,
+  DownloadIcon,
+  MousePointerClickIcon,
+  Redo2Icon,
+  Undo2Icon,
+} from "lucide-react";
+import { BsCloudCheck } from "react-icons/bs";
 import { CiFileOn } from "react-icons/ci";
 
 const Navbar = () => {
@@ -46,6 +53,81 @@ const Navbar = () => {
             <MousePointerClickIcon className="size-4" />
           </Button>
         </Hint>
+        <Hint label="Undo" side={"bottom"} sideOffset={10}>
+          <Button variant={"ghost"} size="icon" onClick={() => {}} className="">
+            <Undo2Icon className="size-4" />
+          </Button>
+        </Hint>
+        <Hint label="Redo" side={"bottom"} sideOffset={10}>
+          <Button variant={"ghost"} size="icon" onClick={() => {}} className="">
+            <Redo2Icon className="size-4" />
+          </Button>
+        </Hint>
+        <Separator orientation="vertical" className="mx-2" />
+        <div className="flex items-center gap-x-2">
+          <BsCloudCheck className="size-[20px] text-muted-foreground" />
+          <div className="text-xs text-muted-foreground">Saved</div>
+        </div>
+        <div className="ml-auto flex items-center gap-x-4">
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <Button variant={"ghost"}>
+                Export <DownloadIcon className="ml-2 size-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="min-w-60">
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}} //TODO: Add functionality
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>JSON</p>
+                  <p className="text-xs text-muted-foreground">
+                    Save for later editing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}} //TODO: Add functionality
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>PNG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Best for sharing on the web
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}} //TODO: Add functionality
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>JPG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Best for printing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}} //TODO: Add functionality
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>SVG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Best for editing in vector software
+                  </p>
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          {/* TODO: Add user button component */}
+        </div>
       </div>
     </nav>
   );

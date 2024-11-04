b105e6fb399ef0f3c5fb6260d93364e3168cdba5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-04 10:04:46 +0300

    Created and rendered the toolbar component

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,6 +2,7 @@
 
 import Navbar from "@/features/editor/components/navbar";
 import Sidebar from "@/features/editor/components/sidebar";
+import Toolbar from "@/features/editor/components/toolbar";
 import { useEditor } from "@/features/editor/hooks/use-editor";
 import { fabric } from "fabric";
 import { useEffect, useRef } from "react";
@@ -26,9 +27,12 @@ const Editor = () => {
       <Navbar />
       <div className="absolute top-[68px] flex h-[calc(100%-68px)] w-full">
         <Sidebar />
-        <div ref={containerRef} className="h-full flex-1 bg-muted">
-          <canvas ref={canvasRef} />
-        </div>
+        <main className="relative flex flex-1 flex-col overflow-auto bg-muted">
+          <Toolbar />
+          <div ref={containerRef} className="h-full flex-1 bg-muted">
+            <canvas ref={canvasRef} />
+          </div>
+        </main>
       </div>
     </div>
   );
---
+++ src/features/editor/components/toolbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Toolbar = () => {
+  return (
+    <div className="z-[49] flex h-[56px] w-full shrink-0 items-center gap-x-2 overflow-x-auto border-b bg-white p-2">
+      Toolbar
+    </div>
+  );
+};
+
+export default Toolbar;

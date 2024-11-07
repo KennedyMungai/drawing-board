8749b84846c40dda54967b67ead8617021737800
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-07 11:16:01 +0300

    Used the use-generate image hook

--- src/features/editor/components/ai-sidebar.tsx
+++ src/features/editor/components/ai-sidebar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Textarea } from "@/components/ui/textarea";
+import { useGenerateImage } from "@/features/ai/api/use-generate-image";
 import ToolSidebarClose from "@/features/editor/components/tool-sidebar-close";
 import ToolSidebarHeader from "@/features/editor/components/tool-sidebar-header";
 import { ActiveTool, Editor } from "@/features/editor/types";
 import { cn } from "@/lib/utils";
+import { FormEvent, useState } from "react";
 
 type Props = {
   activeTool: ActiveTool;
@@ -15,6 +17,24 @@ type Props = {
 const AiSidebar = ({ activeTool, editor, onChangeActiveTool }: Props) => {
   const onClose = () => onChangeActiveTool("select");
 
+  const [value, setValue] = useState("");
+
+  const { mutateAsync, isPending } = useGenerateImage();
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: Block with paywall
+
+    mutateAsync({ json: { prompt: "" } }).then(() => {
+      setValue("");
+
+      editor?.addImage(
+        "https://utfs.io/f/IXChAmYW3Hk9FbF4htGr5VK2c8avmXHDwpNhR4ux0MjiZ7TQ",
+      );
+    });
+  };
+
   return (
     <aside
       className={cn(
@@ -24,15 +44,18 @@ const AiSidebar = ({ activeTool, editor, onChangeActiveTool }: Props) => {
     >
       <ToolSidebarHeader title="AI" description="Generate an image using AI" />
       <ScrollArea>
-        <form className="space-y-4 p-4">
+        <form className="space-y-4 p-4" onSubmit={onSubmit}>
           <Textarea
             cols={30}
             rows={10}
             placeholder="Describe your image"
             required
             minLength={3}
+            value={value}
+            onChange={(editor) => setValue(editor.target.value)}
+            disabled={isPending}
           />
-          <Button className="w-full" type="submit">
+          <Button className="w-full" type="submit" disabled={isPending}>
             Generate
           </Button>
         </form>

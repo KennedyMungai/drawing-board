fa36701d98d812c5f56f079bf1efbb026c84ce87
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-07 09:01:27 +0300

    Deleted the comments

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -2,7 +2,6 @@ import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 
-// Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });

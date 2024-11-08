c858b8ee1ea53f84fc9383dff3c0a51551142afa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-08 09:03:40 +0300

    Implemented the zoom reset logic

--- src/features/editor/components/footer.tsx
+++ src/features/editor/components/footer.tsx
@@ -1,7 +1,7 @@
 import Hint from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { Editor } from "@/features/editor/types";
-import { ZoomInIcon, ZoomOutIcon } from "lucide-react";
+import { MinimizeIcon, ZoomInIcon, ZoomOutIcon } from "lucide-react";
 
 type Props = {
   editor: Editor;
@@ -10,6 +10,11 @@ type Props = {
 const Footer = ({ editor }: Props) => {
   return (
     <footer className="z-[49] flex h-[52px] w-full shrink-0 flex-row-reverse items-center gap-x-1 overflow-x-auto border-t bg-white p-2 px-4">
+      <Hint label="Reset Zoom" side="top" sideOffset={10}>
+        <Button size="icon" variant={"ghost"} onClick={() => editor.autoZoom()}>
+          <MinimizeIcon className="size-5" />
+        </Button>
+      </Hint>
       <Hint label="Zoom In" side="top" sideOffset={10}>
         <Button size="icon" variant={"ghost"} onClick={() => editor.zoomIn()}>
           <ZoomInIcon className="size-5" />

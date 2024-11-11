2418d57b18f733e6383658a66ca9c7aa3ae951ed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 10:13:21 +0300

    Created and rendered the sidebar routes component

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,11 @@
 import Logo from "@/app/(dashboard)/_components/logo";
+import SidebarRoutes from "@/app/(dashboard)/_components/sidebar-routes";
 
 const Sidebar = () => {
   return (
     <aside className="fixed left-0 hidden h-full w-[300px] shrink-0 flex-col lg:flex">
       <Logo />
+      <SidebarRoutes />
     </aside>
   );
 };

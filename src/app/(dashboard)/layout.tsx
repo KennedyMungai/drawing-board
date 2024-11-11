de5a8f5eb3743cb80e8769a4d48234cfa371404b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 09:51:28 +0300

    Created and rendered the dashboard sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
+import Sidebar from "@/app/(dashboard)/_components/sidebar";
 
 type Props = {
   children: ReactNode;
 };
 
 const DashboardLayout = ({ children }: Props) => {
-  return <div className="h-full bg-muted">{children}</div>;
+  return (
+    <div className="h-full bg-muted">
+      <Sidebar />
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;

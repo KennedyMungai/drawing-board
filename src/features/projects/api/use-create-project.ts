7f906c4936b2e21a2ab2d76ed5e9b0058d543ba3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 12:25:10 +0300

    Added the onSuccess and onError callbacks for the hook

--- src/features/projects/api/use-create-project.ts
+++ src/features/projects/api/use-create-project.ts
@@ -1,8 +1,24 @@
+import { client } from "@/lib/hc";
 import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.projects.$post>;
+type RequestType = InferRequestType<typeof client.api.projects.$post>;
 
 export const useCreateProject = () => {
-  const mutation = useMutation({
-    mutationFn: async ({ json }) => {},
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.projects.$post({ json });
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Project created");
+
+      //   TODO: Invalidate project query
+    },
+    onError: () => toast.error("Failed to create project"),
   });
 
   return mutation;

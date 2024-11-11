33f5bdbcc526ea5afbaac150d592610f1cf537f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 12:31:11 +0300

    Explicitly handling error states in the hook

--- src/features/projects/api/use-create-project.ts
+++ src/features/projects/api/use-create-project.ts
@@ -3,13 +3,15 @@ import { useMutation } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { toast } from "sonner";
 
-type ResponseType = InferResponseType<typeof client.api.projects.$post>;
+type ResponseType = InferResponseType<typeof client.api.projects.$post, 200>;
 type RequestType = InferRequestType<typeof client.api.projects.$post>;
 
 export const useCreateProject = () => {
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.projects.$post({ json });
+
+      if (!response.ok) throw new Error("Something went wrong");
 
       return await response.json();
     },

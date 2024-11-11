6d00168c9dfacbd359075a7cc3ff759531cb7a39
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 09:49:43 +0300

    Added a dashboard layout file

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,17 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { signOutAction } from "@/features/auth/actions/auth-action";
 import { protectServer } from "@/features/auth/utils";
 
 const HomePage = async () => {
   await protectServer();
 
-  return (
-    <div>
-      <form action={signOutAction}>
-        <Button>SignOut</Button>
-      </form>
-    </div>
-  );
+  return <div>Home Page</div>;
 };
 
 export default HomePage;

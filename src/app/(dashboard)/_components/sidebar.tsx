057aad8ddcf0d1c11bcedbb01995ce67a05daa2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 09:55:44 +0300

    Made some changes to the dashboard sidebar

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,9 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="fixed left-0 hidden h-full w-[300px] shrink-0 flex-col border-r lg:flex">
+      Sidebar
+    </aside>
+  );
 };
 
 export default Sidebar;
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({ children }: Props) => {
   return (
     <div className="h-full bg-muted">
       <Sidebar />
-      {children}
+      <div className="flex h-full flex-col lg:ml-[300px]">{children}</div>
     </div>
   );
 };

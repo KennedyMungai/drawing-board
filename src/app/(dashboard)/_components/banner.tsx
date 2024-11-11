f09fcde14bed16fb32b5caf1d297201f7520906d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 10:40:26 +0300

    Created and rendered the banner component

---
+++ src/app/(dashboard)/_components/banner.tsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div className="flex aspect-[5/1] min-h-[248px] items-center gap-x-6 rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5] p-6">
+      Banner
+    </div>
+  );
+};
+
+export default Banner;
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,14 @@
+import Banner from "@/app/(dashboard)/_components/banner";
 import { protectServer } from "@/features/auth/utils";
 
 const HomePage = async () => {
   await protectServer();
 
-  return <div>Home Page</div>;
+  return (
+    <div className="mx-auto flex max-w-screen-xl flex-col space-y-6 pb-10">
+      <Banner />
+    </div>
+  );
 };
 
 export default HomePage;

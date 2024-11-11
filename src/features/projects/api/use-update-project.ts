890d7256457687b11de85d91d5e6819a58f84092
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 15:48:45 +0300

    Created teh hoo to update a hook in the database

---
+++ src/features/projects/api/use-update-project.ts
@@ -0,0 +1,38 @@
+import { client } from "@/lib/hc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.projects)[":projectId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.projects)[":projectId"]["$patch"]
+>;
+
+export const useUpdateProject = (projectId: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.projects[":projectId"]["$patch"]({
+        json,
+        param: {
+          projectId,
+        },
+      });
+
+      if (!response.ok) throw new Error("Something went wrong");
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      // queryClient.invalidateQueries({ queryKey: ["projects"] });
+      queryClient.invalidateQueries({ queryKey: ["project", { projectId }] });
+    },
+    onError: () => toast.error("Failed to update project"),
+  });
+
+  return mutation;
+};

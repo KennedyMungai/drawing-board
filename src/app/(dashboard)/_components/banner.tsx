e669175f9101842f7f7d1077f48a54ebd16155cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 12:49:30 +0300

    Added and implemented the hook to create projects

--- src/app/(dashboard)/_components/banner.tsx
+++ src/app/(dashboard)/_components/banner.tsx
@@ -1,7 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCreateProject } from "@/features/projects/api/use-create-project";
 import { ArrowRightIcon, SparklesIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Banner = () => {
+  const { mutate: createProject, isPending: projectCreationPending } =
+    useCreateProject();
+
+  const router = useRouter();
+
+  const onClick = () =>
+    createProject(
+      {
+        json: {
+          name: "Untitled Project",
+          json: "",
+          width: 900,
+          height: 1200,
+        },
+      },
+      {
+        onSuccess: ({ data }) => router.push(`/editor/${data.id}`),
+      },
+    );
+
   return (
     <div className="flex aspect-[5/1] min-h-[248px] items-center gap-x-6 rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5] p-6 text-white">
       <div className="hidden size-28 items-center justify-center rounded-full bg-white/50 lg:flex">
@@ -17,7 +41,12 @@ const Banner = () => {
           Turn inspiration into design in no time.Simply upload an image and let
           AI do the rest.
         </p>
-        <Button variant={"secondary"} className="w-[160px]">
+        <Button
+          variant={"secondary"}
+          className="w-[160px]"
+          onClick={onClick}
+          disabled={projectCreationPending}
+        >
           Start Creating <ArrowRightIcon className="ml-2 size-4" />
         </Button>
       </div>

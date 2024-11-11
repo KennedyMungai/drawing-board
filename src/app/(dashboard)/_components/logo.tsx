0d59d9b034cbfa03d77cddbb6c49a707d054e5d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 10:09:55 +0300

    Created and rendered the logo component

---
+++ src/app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { Space_Grotesk } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const font = Space_Grotesk({ weight: ["700"], subsets: ["latin"] });
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="flex h-[68px] items-center gap-x-2 px-4 transition hover:opacity-75">
+        <div className="relative size-8">
+          <Image src="/logo.png" alt="Logo" width={100} height={100} />
+        </div>
+        <h1 className={cn(font.className, "text-xl font-bold")}>Canva Clone</h1>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,9 @@
+import Logo from "@/app/(dashboard)/_components/logo";
+
 const Sidebar = () => {
   return (
     <aside className="fixed left-0 hidden h-full w-[300px] shrink-0 flex-col lg:flex">
-      Sidebar
+      <Logo />
     </aside>
   );
 };

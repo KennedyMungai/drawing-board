391937c92bb80e01bc2ed0fd2692e983a0ff8ea6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-11 09:41:05 +0300

    Implemented the callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -18,17 +18,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     signIn: "/sign-in",
     signOut: "/sign-out",
   },
-  // callbacks: {
-  //   session({ session, token }) {
-  //     if (token.id) session.user.id = token.id;
+  callbacks: {
+    session({ session, token }) {
+      if (token.id) session.user.id = token.id;
 
-  //     return session;
-  //   },
-  //   jwt({ token, user }) {
-  //     if (user) token.id = user.id;
+      return session;
+    },
+    jwt({ token, user }) {
+      if (user) token.id = user.id;
 
-  //     return token;
-  //   },
-  // },
+      return token;
+    },
+  },
   ...authConfig,
 });

5b7a1da520378bc37cad4fd4e5b97f9f5baf24a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-12 15:17:26 +0300

    Created the hook that could be used to duplicate projects

---
+++ src/features/projects/api/use-duplicate-project.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/hc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.projects)[":projectId"]["duplicate"]["$post"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.projects)[":projectId"]["duplicate"]["$post"]
+>;
+
+export const useDuplicateProject = (projectId: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param }) => {
+      const response = await client.api.projects[":projectId"]["duplicate"][
+        "$post"
+      ]({
+        param,
+      });
+
+      if (!response.ok) throw new Error("Something went wrong");
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["projects"] });
+    },
+    onError: () => toast.error("Failed to duplicate project"),
+  });
+
+  return mutation;
+};

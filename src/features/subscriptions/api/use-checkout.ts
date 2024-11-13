9c31620d5c6f582169583f283131416ffd294c6d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-13 14:37:50 +0300

    Created the hook that calls the hono endpoint

---
+++ src/features/subscriptions/api/use-checkout.ts
@@ -0,0 +1,22 @@
+import { client } from "@/lib/hc";
+import { useMutation } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  typeof client.api.subscriptions.checkout.$post,
+  200
+>;
+
+export const useCheckout = () =>
+  useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.subscriptions.checkout.$post();
+
+      if (!response.ok) throw new Error("Failed to create a session");
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => (window.location.href = "/checkout"),
+    onError: () => toast.error("Failed to create session"),
+  });

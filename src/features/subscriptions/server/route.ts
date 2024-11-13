d7f45697ad783f897b870f782489973e950c16c5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-13 15:38:02 +0300

    Added the stripe webhook endpoint

--- src/features/subscriptions/server/route.ts
+++ src/features/subscriptions/server/route.ts
@@ -1,36 +1,79 @@
+import { db } from "@/db";
+import { subscriptions } from "@/db/schema";
 import { stripe } from "@/lib/stripe";
 import { verifyAuth } from "@hono/auth-js";
 import { Hono } from "hono";
+import Stripe from "stripe";
 
-const app = new Hono().post("/checkout", verifyAuth(), async (c) => {
-  const auth = c.get("authUser");
+const app = new Hono()
+  .post("/checkout", verifyAuth(), async (c) => {
+    const auth = c.get("authUser");
 
-  if (!auth.token?.id) return c.json({ error: "Unauthorized" }, 401);
+    if (!auth.token?.id) return c.json({ error: "Unauthorized" }, 401);
 
-  // @ts-expect-error Still works with the error
-  const session = await stripe.checkout.sessions.create({
-    success_url: `${process.env.NEXT_PUBLIC_APP_URL}?success=true`,
-    cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}?success=false`,
-    payment_method_types: ["card", "paypal"],
-    mode: "subscription",
-    billing_address_collection: "auto",
-    customer_email: auth.token.email ?? "",
-    line_items: [
-      {
-        price: process.env.STRIPE_PRICE_ID,
-        quantity: 1,
+    // @ts-expect-error Still works with the error
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}?success=true`,
+      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}?success=false`,
+      payment_method_types: ["card", "paypal"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: auth.token.email ?? "",
+      line_items: [
+        {
+          price: process.env.STRIPE_PRICE_ID,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: auth.token.id,
       },
-    ],
-    metadata: {
-      userId: auth.token.id,
-    },
+    });
+
+    const url = session.url;
+
+    if (!url) return c.json({ error: "Failed to create session" }, 400);
+
+    return c.json({ data: url });
+  })
+  .post("/webhook", async (c) => {
+    const body = await c.req.text();
+    const signature = c.req.header("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        body,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET!,
+      );
+    } catch (error: unknown) {
+      // @ts-expect-error The error has an unknown type
+      return c.json({ error: `Invalid signatures ${error.message}` });
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    if (event.type === "checkout.session.completed") {
+      const subscription = await stripe.subscriptions.retrieve(
+        session.subscription as string,
+      );
+
+      if (!session?.metadata?.userId)
+        return c.json({ error: "Invalid session" }, 400);
+
+      await db.insert(subscriptions).values({
+        status: subscription.status,
+        userId: session.metadata.userId,
+        subscriptionId: subscription.id,
+        customerId: subscription.customer as string,
+        priceId: subscription.items.data[0].price.product as string,
+        currentPeriodEnd: new Date(subscription.current_period_end * 1000),
+      });
+
+      return c.json(null, 200);
+    }
   });
-
-  const url = session.url;
-
-  if (!url) return c.json({ error: "Failed to create session" }, 400);
-
-  return c.json({ data: url });
-});
 
 export default app;

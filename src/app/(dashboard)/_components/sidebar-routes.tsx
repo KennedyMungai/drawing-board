27d48eb48541e398656a16a60aada9dd800608b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-14 09:26:47 +0300

    Using the useCheckout hook

--- src/app/(dashboard)/_components/sidebar-routes.tsx
+++ src/app/(dashboard)/_components/sidebar-routes.tsx
@@ -3,6 +3,7 @@
 import SidebarItem from "@/app/(dashboard)/_components/sidebar-item";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { useCheckout } from "@/features/subscriptions/api/use-checkout";
 import { usePaywall } from "@/features/subscriptions/hooks/use-paywall";
 import {
   CreditCardIcon,
@@ -15,22 +16,30 @@ import { usePathname } from "next/navigation";
 const SidebarRoutes = () => {
   const pathname = usePathname();
 
-  const { triggerPaywall } = usePaywall();
+  const { mutate: checkout, isPending: isCheckoutPending } = useCheckout();
+
+  const { shouldBlock, isLoading } = usePaywall();
 
   return (
     <div className="flex flex-1 flex-col gap-y-4">
-      <div className="px-4">
-        <Button
-          onClick={triggerPaywall}
-          className="w-full rounded-xl border-none transition hover:bg-white hover:opacity-75"
-          variant={"outline"}
-          size="lg"
-        >
-          <CrownIcon className="mr-2 size-4 fill-yellow-500 text-yellow-500" />{" "}
-          Upgrade to Pro
-        </Button>
-      </div>
-      <Separator className="px-3" />
+      {shouldBlock && !isLoading && (
+        <>
+          <div className="px-3">
+            <Button
+              onClick={() => checkout()}
+              disabled={isCheckoutPending}
+              className="w-full rounded-xl border-none transition hover:bg-white hover:opacity-75"
+              variant={"outline"}
+              size="lg"
+            >
+              <CrownIcon className="mr-2 size-4 fill-yellow-500 text-yellow-500" />{" "}
+              Upgrade to Pro
+            </Button>
+          </div>
+          <Separator className="px-3" />
+        </>
+      )}
+
       <ul className="flex flex-col gap-y-1 px-3">
         <SidebarItem
           href="/"

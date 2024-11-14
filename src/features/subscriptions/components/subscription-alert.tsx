32dfe81c92e043ded74c2ebfac794df24b60f1e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-14 10:58:24 +0300

    Implemented the logic for the success scenario

--- src/features/subscriptions/components/subscription-alert.tsx
+++ src/features/subscriptions/components/subscription-alert.tsx
@@ -1,23 +1,28 @@
 "use client";
 
 import { useFailModal } from "@/features/subscriptions/store/use-fail-modal";
+import { useSuccessModal } from "@/features/subscriptions/store/use-success-modal";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const SubscriptionAlert = () => {
   const { onOpen: onOpenFail } = useFailModal();
+  const { onOpen: onOpenSuccess } = useSuccessModal();
 
   const [isMounted, setIsMounted] = useState(false);
 
   const searchParams = useSearchParams();
 
   const cancelled = searchParams.get("success") === "false";
+  const success = searchParams.get("success") === "true";
 
   useEffect(() => setIsMounted(true), []);
 
   useEffect(() => {
     if (cancelled) onOpenFail();
-  }, [cancelled, onOpenFail]);
+
+    if (success) onOpenSuccess();
+  }, [cancelled, onOpenFail, success, onOpenSuccess]);
 
   if (!isMounted) return null;
 
